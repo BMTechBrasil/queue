2368ddd0936c22bb126ff2ff2c4e71adbf5be85a
BMTechBrasil <helpdesk@bmtechbrasil.com.br>
2018-02-06 18:14:50 -0200

    finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do 
 
-  get 'users/new'
-
 	root 'requests#index' ,as: 'home'
-
 	get '/signup', to: 'users#new'
-
-	resource :requests do
-	end
+	post '/signup', to: 'users#create'
+	resources :users
 
 end
