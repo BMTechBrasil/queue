bdc7ac592e7e504adb07a6abf79ccc4bf2e8ded3
BMTechBrasil <helpdesk@bmtechbrasil.com.br>
2018-02-06 13:50:30 -0200

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do 
+
+  get 'users/new'
+
+	root 'requests#index' ,as: 'home'
+
+	get '/signup', to: 'users#new'
+
+	resource :requests do
+	end
+
 end
